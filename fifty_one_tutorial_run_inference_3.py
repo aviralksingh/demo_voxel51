d2aa89c1e9c895beec523d314743ebebaa1a436c
Aviral Singh <aviralksingh@gmail.com>
2025-11-28 18:13:22 -0500

    dinov3 embedding working

--- fifty_one_tutorial_run_inference_3.py
+++ fifty_one_tutorial_run_inference_3.py
@@ -2,7 +2,14 @@ import fiftyone as fo
 import fiftyone.brain as fob
 import fiftyone.utils.transformers as fouhft
 import transformers
+import fiftyone.zoo as foz
 
+# You can load your own dataset
+dataset = foz.load_zoo_dataset(
+    "https://github.com/voxel51/coco-2017",
+    split="validation",
+    dataset_name="coco-2017-validation",
+)
 print(f"list of datasets: {fo.list_datasets()}")
 
 dataset = fo.load_dataset("coco-2017-validation")
@@ -24,4 +31,6 @@ viz = fob.compute_visualization(
     brain_key="dino_dense_umap"
 )
 session = fo.launch_app(dataset, port=5151)
+
 print(session.url)
+session.wait() 
